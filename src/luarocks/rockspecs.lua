7ec84e6acfcbd0293de25b95fae91a5ebcc0e525
Vhyrro <vhyrro@gmail.com>
2024-01-04 21:07:07 +0100

    docs(src/luarocks/rockspecs): add luacats annotations

--- src/luarocks/rockspecs.lua
+++ src/luarocks/rockspecs.lua
@@ -8,6 +8,7 @@ local type_rockspec = require("luarocks.type.rockspec")
 local util = require("luarocks.util")
 local vers = require("luarocks.core.vers")
 
+---@enum (key) VendoredBuildType
 local vendored_build_type_set = {
    ["builtin"] = true,
    ["cmake"] = true,
@@ -17,6 +18,59 @@ local vendored_build_type_set = {
    ["none"] = true,
 }
 
+---@class rockspec.build.install
+---@field lua?  table<string, any> Lua modules written in Lua.
+---@field lib?  table<string, any> Dynamic libraries implemented compiled Lua modules.
+---@field conf? table<string, any> Configuration files.
+---@field bin?  table<string, any> Lua command-line scripts.
+
+---@class rockspec.build
+---@field type              VendoredBuildType|string The LuaRocks build back-end to use.
+---@field install?          rockspec.build.install Installation instructions for LuaRocks.
+---@field copy_directories? string[] List of directories in the source directory to be copied to the rock installation prefix as-is. Useful for installing documentation and other files such as samples and tests. Default is {"doc"} for documentation to be locally installed in the rocktree.
+---@field patches?          table<string, string>
+
+---@class (exact) rockspec.source
+---@field url      string The URL of the package source archive.
+---@field md5?     string The MD5 sum for the source archive.
+---@field file?    string The filename of the source archive. Can be omitted if it can be inferred from the `source.url` field.
+---@field dir?     string The name of the directory created when the source archive is unpacked. Can be omitted if it can be inferred from the `source.file` field.
+---@field cvs?     string For SCM-based URL protocols such as "cvs://" and "git://", this field can be used to specify a tag for checking out sources.
+---@field cvs_tag? string Deprecated, backwards-compatible alternative to `source.cvs`.
+---@field branch?  string For SCM-based URL protocols such as "git://", this field can be used to specify a branch for checking out sources.
+---@field module?  string For SCM-based URL protocols such as "cvs://" and "git://", this field can be used to specify the module to be checked out. Can be omitted if it is the same as the basename of the `source.url` field.
+
+---@class (exact) rockspec.description
+---@field summary?    string A one-line description of the package.
+---@field detailed?   string A longer description of the package.
+---@field license?    string The license used by the package. A short name, such as "MIT" and "GPL-2" is preferred. Software using the same license as Lua 5.x should use "MIT".
+---@field homepage?   string An URL for the project. This is not the URL for the tarball, but the address of a website.
+---@field issues_url? string An URL for the project's issue tracker.
+---@field maintainer? string Contact information for the rockspec maintainer (which may or may not be the package maintainer - contact for the package maintainer can usually be obtained through the address in the homepage field).
+---@field labels?     string[] A list of short strings that specify labels for categorization of this rock. See the list of labels at [http://luarocks.org] for inspiration.
+
+---@alias Dependencies string[]
+---@alias ExternalDependencies table<string, { header?: string, library?: string }>
+
+---@class (exact) rockspec
+---@field build? rockspec.build
+---@field build_dependencies? Dependencies
+---@field dependencies? Dependencies
+---@field external_dependencies? ExternalDependencies
+---@field format_is_at_least function
+---@field hooks? unknown
+---@field local_abs_filename? any
+---@field name? string
+---@field package string
+---@field rocks_provided? table<string, string>
+---@field rockspec_format? string
+---@field source rockspec.source
+---@field test table
+---@field test_dependencies Dependencies
+---@field version string
+---@field description? rockspec.description
+
+---@type metatable
 local rockspec_mt = {}
 
 rockspec_mt.__index = rockspec_mt
@@ -88,6 +142,7 @@ local function configure_paths(rockspec)
    rockspec.variables = vars
 end
 
+---@param rockspec rockspec
 function rockspecs.from_persisted_table(filename, rockspec, globals, quick)
    assert(type(rockspec) == "table")
    assert(type(globals) == "table" or globals == nil)
@@ -118,6 +173,7 @@ function rockspecs.from_persisted_table(filename, rockspec, globals, quick)
       end
    end
 
+   -- rockspec.build.copy_directories
    platform_overrides(rockspec.build)
    platform_overrides(rockspec.dependencies)
    platform_overrides(rockspec.build_dependencies)
